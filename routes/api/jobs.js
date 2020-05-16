c2444caa879b0787c8251ecff2435040141893d6
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-05-16 18:19:21 +0800

    finished crud

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -1,17 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const Job = require("../../models/Job");
-const uuid = require("uuid");
 
 // @route GET api/jobs
 // @desc Get all jobs
 // @access Public
+
 router.get("/", async (req, res) => {
   try {
     const jobs = await Job.find();
     res.json(jobs);
   } catch (err) {
     console.error(err);
+    res.status(404).json("404 Not Found");
+  }
+});
+
+// @route GET api/jobs/search
+// @desc Search Jobs
+// @access Public
+
+router.get("/search", async (req, res) => {
+  try {
+    const q = req.query.q;
+    const jobs = await Job.find({});
+    res.json(jobs);
+  } catch (err) {
+    console.error(err);
+    res.status(404).json("404 Not Found");
   }
 });
 
@@ -25,7 +41,7 @@ router.get("/:id", async (req, res) => {
     res.json(job);
   } catch (err) {
     console.error(err);
-    res.status(404).json("Not Found");
+    res.status(404).json("404 Not Found");
   }
 });
 
@@ -59,4 +75,31 @@ router.post("/create", async (req, res) => {
   }
 });
 
+// @route PUT api/jobs/:id
+// @desc Update a Job
+// @access Public
+
+router.put("/:id", async (req, res) => {
+  try {
+    await Job.updateOne({ _id: req.params.id }, { ...req.body });
+    res.json("Updated Successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(404).json("404 Not Found");
+  }
+});
+
+// @route Delete api/jobs/:id
+// @desc Delete a Job
+// @access Public
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Job.deleteOne({ _id: req.params.id });
+    res.json("Deleted Successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(404).json("404 Not Found");
+  }
+});
 module.exports = router;
