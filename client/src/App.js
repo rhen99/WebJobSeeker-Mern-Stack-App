9c04d19497b0c9a0df2c7e33d99abaf272b1cff6
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-05-30 18:20:46 +0800

    decided to use bootstrao

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.scss";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.scss";
+
+function Navbar() {
+  return (
+    <>
+      <nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
