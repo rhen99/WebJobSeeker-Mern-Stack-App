01fa44e7c25dd3146e4c54d6e14b62f8d736d561
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-06-01 11:13:48 +0800

    moved the bootstrap.min.css to src

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./App.scss";
+import "./bootstrap.min.css";
 import Navbar from "./components/Navbar";
 
 function App() {
